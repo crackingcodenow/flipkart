1841cc64276e9d647bb60d0f21fd3d613098ebc6
KRISHNA KOUSHIK <crackingcodenow@gmail.com>
2019-11-20 19:32:16 +0530

    few ffiles delted

--- index.js
+++
@@ -1,6 +0,0 @@
-function add() {
-    var a = 5, b = 3, c;
-    c = a + b;
-    console.log(c);
-}
-add();
--- index.ts
+++
@@ -1,7 +0,0 @@
-function add:number (a:number, b:number) {
-  let a: number , b: number, c: number;
-  c = a + b;
-  console.log(c);
-}
-
-add(a, b);
